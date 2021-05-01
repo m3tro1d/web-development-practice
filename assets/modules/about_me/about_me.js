c769378c4f37caac3aa3029e688552a079c956b3
m3tro1d <qazwf734@gmail.com>
2021-05-01 12:05:48 +0300

    JS refactoring

--- assets/modules/about_me/about_me.js
+++ assets/modules/about_me/about_me.js
@@ -5,8 +5,8 @@ import Slider from '../components/slider/slider';
 
 // Iterate through all the .slider blocks and make them dynamic
 $(document).ready(() => {
-    $('.slider').each((i, obj) => {
-        const slider = new Slider($(obj));
-        slider.initialize();
-    })
+  $('.slider').each((i, obj) => {
+    const slider = new Slider($(obj));
+    slider.initialize();
+  })
 });
--- assets/modules/components/slider/slider.js
+++ assets/modules/components/slider/slider.js
@@ -3,97 +3,97 @@ import './slider.css';
 import $ from 'jquery';
 
 export default class Slider {
-    constructor(object) {
-        // Slider objects
-        this.master = $(object);
-        this.viewport = $(object.children()[0]);
-        this.wrapper = $(this.viewport.children()[0]);
-        this.prev_btn = $(this.viewport.children()[1]);
-        this.next_btn = $(this.viewport.children()[2]);
-        this.nav_btns = $(this.viewport.children()[3]).children();
+  constructor(object) {
+    // Slider objects
+    this._master = $(object);
+    this._viewport = $(object.children()[0]);
+    this._wrapper = $(this._viewport.children()[0]);
+    this._prev_btn = $(this._viewport.children()[1]);
+    this._next_btn = $(this._viewport.children()[2]);
+    this._nav_btns = $(this._viewport.children()[3]).children();
 
-        // Slider info
-        this.currentSlide = 1;
-        this.slideAmount = this.wrapper.children().length;
-        this.sliderInterval = 2000;
-        this.navBtnId = 0;
-    }
+    // Slider info
+    this._currentSlide = 1;
+    this._slideAmount = this._wrapper.children().length;
+    this._sliderInterval = 2000;
+    this._navBtnId = 0;
+  }
 
-    initialize() {
-        this._startSlider();
-        this._registerButtonEvents();
-        this._registerNavButtonsEvents();
-    }
+  initialize() {
+    this._startSlider();
+    this._registerButtonEvents();
+    this._registerNavButtonsEvents();
+  }
 
-    _startSlider() {
-        this.switchInterval = setInterval(this._nextSlide.bind(this), this.sliderInterval);
-        const onHoverUp = (function() {
-            clearInterval(this.switchInterval);
-        }).bind(this);
-        const onHover = (function() {
-            this.switchInterval = setInterval(this._nextSlide.bind(this), this.sliderInterval);
-        }).bind(this);
-        this.viewport.hover(onHoverUp, onHover);
-    }
+  _startSlider() {
+    this._switchInterval = setInterval(this._nextSlide.bind(this), this._sliderInterval);
+    const onHoverUp = (function() {
+      clearInterval(this._switchInterval);
+    }).bind(this);
+    const onHover = (function() {
+      this._switchInterval = setInterval(this._nextSlide.bind(this), this._sliderInterval);
+    }).bind(this);
+    this._viewport.hover(onHoverUp, onHover);
+  }
 
-    _registerButtonEvents() {
-        this.next_btn.click(this._nextSlide.bind(this));
-        this.prev_btn.click(this._prevSlide.bind(this));
-    }
+  _registerButtonEvents() {
+    this._next_btn.click(this._nextSlide.bind(this));
+    this._prev_btn.click(this._prevSlide.bind(this));
+  }
 
-    _registerNavButtonsEvents() {
-        // Save the slider as 'that', because we need 'this' for the event button id
-        const that = this;
-        const onClick = function() {
-            let translateWidth;
-            that.navBtnId = $(this).index();
-            if (that.navBtnId + 1 != that.currentSlide) {
-                translateWidth = -that.viewport.width() * (that.navBtnId);
-                that.wrapper.css({
-                    'transform': 'translate(' + translateWidth + 'px, 0)',
-                    '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-                    '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-                });
-                that.currentSlide = that.navBtnId + 1;
-            }
-        }
-        this.nav_btns.click(onClick);
+  _registerNavButtonsEvents() {
+    // Save the slider as 'that', because we need 'this' for the event button id
+    const that = this;
+    const onClick = function() {
+      let translateWidth;
+      that._navBtnId = $(this).index();
+      if (that._navBtnId + 1 != that._currentSlide) {
+        translateWidth = -that._viewport.width() * (that._navBtnId);
+        that._wrapper.css({
+          'transform': 'translate(' + translateWidth + 'px, 0)',
+          '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+          '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        that._currentSlide = that._navBtnId + 1;
+      }
     }
+    this._nav_btns.click(onClick);
+  }
 
-    _nextSlide() {
-        let translateWidth;
-        if (this.currentSlide == this.slideAmount || this.currentSlide <= 0 || this.currentSlide > this.slideAmount) {
-            this.wrapper.css('transform', 'translate(0, 0)');
-            this.currentSlide = 1;
-        } else {
-            translateWidth = -this.viewport.width() * (this.currentSlide);
-            this.wrapper.css({
-                'transform': 'translate(' + translateWidth + 'px, 0)',
-                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-            });
-            this.currentSlide++;
-        }
+  _nextSlide() {
+    let translateWidth;
+    if (this._currentSlide == this._slideAmount || this._currentSlide <= 0 || this._currentSlide > this._slideAmount) {
+      this._wrapper.css('transform', 'translate(0, 0)');
+      this._currentSlide = 1;
+    } else {
+      translateWidth = -this._viewport.width() * (this._currentSlide);
+      this._wrapper.css({
+        'transform': 'translate(' + translateWidth + 'px, 0)',
+        '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+        '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+      });
+      this._currentSlide++;
     }
+  }
 
-    _prevSlide() {
-        let translateWidth;
-        if (this.currentSlide == 1 || this.currentSlide <= 0 || this.currentSlide > this.slideAmount) {
-            translateWidth = -this.viewport.width() * (this.slideAmount - 1);
-            this.wrapper.css({
-                'transform': 'translate(' + translateWidth + 'px, 0)',
-                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-            });
-            this.currentSlide = this.slideAmount;
-        } else {
-            translateWidth = -this.viewport.width() * (this.currentSlide - 2);
-            this.wrapper.css({
-                'transform': 'translate(' + translateWidth + 'px, 0)',
-                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-            });
-            this.currentSlide--;
-        }
+  _prevSlide() {
+    let translateWidth;
+    if (this._currentSlide == 1 || this._currentSlide <= 0 || this._currentSlide > this._slideAmount) {
+      translateWidth = -this._viewport.width() * (this._slideAmount - 1);
+      this._wrapper.css({
+        'transform': 'translate(' + translateWidth + 'px, 0)',
+        '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+        '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+      });
+      this._currentSlide = this._slideAmount;
+    } else {
+      translateWidth = -this._viewport.width() * (this._currentSlide - 2);
+      this._wrapper.css({
+        'transform': 'translate(' + translateWidth + 'px, 0)',
+        '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+        '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+      });
+      this._currentSlide--;
     }
+  }
 }
