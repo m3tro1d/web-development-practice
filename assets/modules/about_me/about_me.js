b222e35904911298562cf21e4a7e182623074c6c
m3tro1d <qazwf734@gmail.com>
2021-05-03 11:39:55 +0300

    Add button for images updating

--- assets/modules/about_me/about_me.js
+++ assets/modules/about_me/about_me.js
@@ -3,10 +3,21 @@ import './about_me.css';
 import $ from 'jquery';
 import Slider from '../components/slider/slider';
 
-// Iterate through all the .slider blocks and make them dynamic
 $(document).ready(() => {
+  // Iterate through all the .slider blocks and make them dynamic
   $('.slider').each((i, obj) => {
     const slider = new Slider($(obj));
     slider.initialize();
   })
+
+  // Add update button handler
+  $('#update-btn').click(() => {
+    $.ajax({
+      url: '/update',
+      method: 'POST',
+    })
+      .done(res => {
+        console.log(res);
+      });
+  });
 });
