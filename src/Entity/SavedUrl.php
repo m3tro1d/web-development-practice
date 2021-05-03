96841687544ff288929a9aab7a2348e6b0fd5453
m3tro1d <qazwf734@gmail.com>
2021-05-03 11:16:34 +0300

    Add SavedUrl entity and repository

---
+++ src/Entity/SavedUrl.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SavedUrlRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SavedUrlRepository::class)
+ */
+class SavedUrl
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=511)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $keyword;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getKeyword(): ?string
+    {
+        return $this->keyword;
+    }
+
+    public function setKeyword(string $keyword): self
+    {
+        $this->keyword = $keyword;
+
+        return $this;
+    }
+}
