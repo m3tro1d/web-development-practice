00f33080f6d2be394f681b532226ace4ff20c12d
m3tro1d <qazwf734@gmail.com>
2021-05-06 18:55:15 +0300

    Edit doctrine config and renew the migrations

--- migrations/Version20210506154015.php
+++ migrations/Version20210506154015.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210503152601 extends AbstractMigration
+final class Version20210506154015 extends AbstractMigration
 {
     public function getDescription() : string
     {
